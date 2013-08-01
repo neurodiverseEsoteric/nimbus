61f10934b1bae992af5da54390507538b8456630
Daniel Sim <foxhead128@Mishiko.(none)>
2013-08-01 16:02:31 -0400

    Corrected the keywords popup in universal search.

--- extensions/universal search/script.js
+++ extensions/universal search/script.js
@@ -1,17 +1,17 @@
 var nimbuskws = new Object();
+nimbuskws['d '] = "https://duckduckgo.com/?q=\%s";
+nimbuskws['w '] = "https://en.wikipedia.org/w/index.php?search=\%s&title=Special\%3ASearch";
 nimbuskws['g '] = "http://www.google.com/search?q=\%s";
 nimbuskws['i '] = "http://www.google.com/search?tbm=isch&q=\%s";
 nimbuskws['m '] = "http://www.googlemusicsearch.com/search?q=\%s";
 nimbuskws['b '] = "http://www.bing.com/search?q=\%s";
 nimbuskws['y '] = "http://search.yahoo.com/search?p=\%s";
-nimbuskws['d '] = "https://duckduckgo.com/?q=\%s";
 nimbuskws['da '] = "http://www.deviantart.com/?q=\%s";
-nimbuskws['w '] = "https://en.wikipedia.org/w/index.php?search=\%s&title=Special\%3ASearch";
 var nimbussterms = "k";
 while (nimbussterms == "k") {
     nimbussterms = prompt('Search (enter k for keywords):',"");
     if (nimbussterms == "k") {
-        window.alert('Keywords:\nd: DuckDuckGo\nw: Wikipedia\nw: Yahoo\ng: Google Search\ni: Google Image Search\nm: Google Music Search\nb: Bing\ny: Yahoo');
+        window.alert('Keywords:\nd: DuckDuckGo\nw: Wikipedia\ng: Google Search\ni: Google Image Search\nm: Google Music Search\nb: Bing\ny: Yahoo\nda: deviantART');
     }
     else {
         var kwfalse = true;
