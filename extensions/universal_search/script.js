b6b326aef8c8ae2ddaaf1e48bce4b164d459261c
Daniel Sim <foxhead128@Mishiko.(none)>
2013-08-08 00:46:35 -0400

    Added some more engines to universal_search extension.

--- extensions/universal_search/script.js
+++ extensions/universal_search/script.js
@@ -1,9 +1,12 @@
 var nimbuskws = new Object();
 nimbuskws['d '] = "https://duckduckgo.com/?q=\%s";
 nimbuskws['w '] = "https://en.wikipedia.org/w/index.php?search=\%s&title=Special\%3ASearch";
+nimbuskws['wk '] = "http://en.wiktionary.org/w/index.php?search=\%s&title=Special:Search";
 nimbuskws['g '] = "http://www.google.com/search?q=\%s";
 nimbuskws['i '] = "http://www.google.com/search?tbm=isch&q=\%s";
 nimbuskws['m '] = "http://www.googlemusicsearch.com/search?q=\%s";
+nimbuskws['tr '] = "http://translate.google.com/#auto/en/\%s";
+nimbuskws['t '] = "http://thesaurus.com/browse/\%s";
 nimbuskws['b '] = "http://www.bing.com/search?q=\%s";
 nimbuskws['y '] = "http://search.yahoo.com/search?p=\%s";
 nimbuskws['da '] = "http://www.deviantart.com/?q=\%s";
@@ -11,7 +14,7 @@ var nimbussterms = "k";
 while (nimbussterms == "k") {
     nimbussterms = prompt('Search (enter k for keywords):',"");
     if (nimbussterms == "k") {
-        window.alert('Keywords:\nd: DuckDuckGo\nw: Wikipedia\ng: Google Search\ni: Google Image Search\nm: Google Music Search\nb: Bing\ny: Yahoo\nda: deviantART');
+        window.alert('Keywords:\nd: DuckDuckGo\nw: Wikipedia\nwk: Wiktionary\ng: Google Search\ni: Google Image Search\nm: Google Music Search\ntr: Google Translate\nt: Thesaurus.com\nb: Bing\ny: Yahoo\nda: deviantART');
     }
     else {
         var kwfalse = true;
