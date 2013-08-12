b90be04bb2fec2da2f01eda7640633e9bc3099d4
Daniel Sim <foxhead128@Mishiko.(none)>
2013-08-12 00:58:27 -0400

    Added an incomplete zh_CN translation hacked together using Ryouko's old zh_CN translation.

---
+++ translations/zh_CN.ts
@@ -0,0 +1,297 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>General</name>
+    <message>
+        <source>&lt;h3&gt;Nimbus&lt;/h3&gt;Python 3/Qt 4-based web browser.</source>
+        <translation type="unfinished">&lt;h3&gt;Nimbus&lt;/h3&gt;Python 3/Qt 4-based web browser.</translation>
+    </message>
+    <message>
+        <source>&lt;b&gt;Proxy configuration&lt;/b&gt;</source>
+        <translation type="unfinished">&lt;b&gt;Proxy configuration&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <source>Abort</source>
+        <translation>退出</translation>
+    </message>
+    <message>
+        <source>About &amp;Qt</source>
+        <translation type="unfinished">关于Qt(&amp;Q)</translation>
+    </message>
+    <message>
+        <source>A&amp;bout Nimbus</source>
+        <translation>关于Nimbus(&amp;A)</translation>
+    </message>
+    <message>
+        <source>All files (*)</source>
+        <translation type="unfinished">All files (*)</translation>
+    </message>
+    <message>
+        <source>&amp;Apply</source>
+        <translation>应用(&amp;A)</translation>
+    </message>
+    <message>
+        <source>Automatically load &amp;images</source>
+        <translation>自动载入图片(&amp;I)</translation>
+    </message>
+    <message>
+        <source>Cache</source>
+        <translation type="unfinished">Cache</translation>
+    </message>
+    <message>
+        <source>Clear</source>
+        <translation>清除</translation>
+    </message>
+    <message>
+        <source>&amp;Clear Data...</source>
+        <translation>清除历史(&amp;C)</translation>
+    </message>
+    <message>
+        <source>Clear Data</source>
+        <translation>清除历史</translation>
+    </message>
+    <message>
+        <source>Close</source>
+        <translation>关闭</translation>
+    </message>
+    <message>
+        <source>Close &amp;window with last tab</source>
+        <translation type="unfinished">Close &amp;window with last tab</translation>
+    </message>
+    <message>
+        <source>&amp;Content</source>
+        <translation>内容(&amp;C)</translation>
+    </message>
+    <message>
+        <source>Cookies</source>
+        <translation type="unfinished">Cookies</translation>
+    </message>
+    <message>
+        <source>Disabled extensions:</source>
+        <translation>扩展已禁用</translation>
+    </message>
+    <message>
+        <source>Download complete: %s</source>
+        <translation type="unfinished">文件已经成功下载!耶! %s</translation>
+    </message>
+    <message>
+        <source>Download complete</source>
+        <translation type="unfinished">文件已经成功下载!耶!</translation>
+    </message>
+    <message>
+        <source>Enable ad &amp;blocking</source>
+        <translation>激活Ad Block(&amp;B)</translation>
+    </message>
+    <message>
+        <source>Enable DNS &amp;prefetching</source>
+        <translation type="unfinished">Enable DNS &amp;prefetching</translation>
+    </message>
+    <message>
+        <source>Enable &amp;Java</source>
+        <translation>激活Java(&amp;J)</translation>
+    </message>
+    <message>
+        <source>Enable Java&amp;Script</source>
+        <translation>激活Java脚本(&amp;S)</translation>
+    </message>
+    <message>
+        <source>Enable &amp;plugins</source>
+        <translation>激活插件(&amp;P)</translation>
+    </message>
+    <message>
+        <source>Enable site specific &amp;quirks</source>
+        <translation type="unfinished">Enable site specific &amp;quirks</translation>
+    </message>
+    <message>
+        <source>Enable tiled backing store</source>
+        <translation type="unfinished">Enable tiled backing store</translation>
+    </message>
+    <message>
+        <source>Enable &amp;XSS auditing</source>
+        <translation type="unfinished">Enable &amp;XSS auditing</translation>
+    </message>
+    <message>
+        <source>Enabled extensions:</source>
+        <translation>激活扩展</translation>
+    </message>
+    <message>
+        <source>Everything</source>
+        <translation>所有</translation>
+    </message>
+    <message>
+        <source>Expand subframes to fit contents</source>
+        <translation type="unfinished">Expand subframes to fit contents</translation>
+    </message>
+    <message>
+        <source>&amp;Extensions</source>
+        <translation>扩展(&amp;E)</translation>
+    </message>
+    <message>
+        <source>Find</source>
+        <translation>查找</translation>
+    </message>
+    <message>
+        <source>&amp;Find...</source>
+        <translation>查找(&amp;F)</translation>
+    </message>
+    <message>
+        <source>Find Ne&amp;xt</source>
+        <translation>再次查找(&amp;X)</translation>
+    </message>
+    <message>
+        <source>Find Pre&amp;vious</source>
+        <translation>查找上一个(&amp;V)</translation>
+    </message>
+    <message>
+        <source>&amp;General</source>
+        <translation>通用(&amp;G)</translation>
+    </message>
+    <message>
+        <source>Go Home</source>
+        <translation type="unfinished">Go Home</translation>
+    </message>
+    <message>
+        <source>History</source>
+        <translation>历史</translation>
+    </message>
+    <message>
+        <source>Homepage:</source>
+        <translation type="unfinished">Homepage:</translation>
+    </message>
+    <message>
+        <source>Hostname:</source>
+        <translation>主机名</translation>
+    </message>
+    <message>
+        <source>Maximum cache size:</source>
+        <translation type="unfinished">Maximum cache size:</translation>
+    </message>
+    <message>
+        <source>&amp;Menu</source>
+        <translation>菜单(&amp;M)</translation>
+    </message>
+    <message>
+        <source>&amp;Network</source>
+        <translation type="unfinished">&amp;Network</translation>
+    </message>
+    <message>
+        <source>New Tab</source>
+        <translation>新建页签</translation>
+    </message>
+    <message>
+        <source>New &amp;Incognito Tab</source>
+        <translation>新建隐私浏览页签(&amp;I)</translation>
+    </message>
+    <message>
+        <source>New &amp;Tab</source>
+        <translation>新建页签(&amp;T)</translation>
+    </message>
+    <message>
+        <source>&amp;New Window</source>
+        <translation>新建视窗(&amp;N)</translation>
+    </message>
+    <message>
+        <source>Nimbus</source>
+        <translation type="unfinished">Nimbus</translation>
+    </message>
+    <message>
+        <source>Number of reopenable tabs:</source>
+        <translation>撤销页签关闭的最大数目</translation>
+    </message>
+    <message>
+        <source>Number of reopenable windows:</source>
+        <translation type="unfinished">Number of reopenable windows:</translation>
+    </message>
+    <message>
+        <source>Open &amp;settings in tab</source>
+        <translation type="unfinished">Open &amp;settings in tab</translation>
+    </message>
+    <message>
+        <source>Password:</source>
+        <translation>密码</translation>
+    </message>
+    <message>
+        <source>Persistent Storage</source>
+        <translation>长期存储</translation>
+    </message>
+    <message>
+        <source>Port:</source>
+        <translation>端口</translation>
+    </message>
+    <message>
+        <source>&amp;Print...</source>
+        <translation>打印(&amp;P)</translation>
+    </message>
+    <message>
+        <source>Print e&amp;lement backgrounds</source>
+        <translation type="unfinished">Print e&amp;lement backgrounds</translation>
+    </message>
+    <message>
+        <source>Print Previe&amp;w</source>
+        <translation>打印预览(&amp;W)</translation>
+    </message>
+    <message>
+        <source>Quit</source>
+        <translation>退出</translation>
+    </message>
+    <message>
+        <source>&amp;Reload</source>
+        <translation type="unfinished">&amp;Reload</translation>
+    </message>
+    <message>
+        <source>Remember &amp;history</source>
+        <translation type="unfinished">Remember &amp;history</translation>
+    </message>
+    <message>
+        <source>&amp;Reopen Tab</source>
+        <translation>撤销关闭页签(&amp;R)</translation>
+    </message>
+    <message>
+        <source>R&amp;eopen Window</source>
+        <translation>撤销关闭视窗(&amp;E)</translation>
+    </message>
+    <message>
+        <source>Save As...</source>
+        <translation>保存为</translation>
+    </message>
+    <message>
+        <source>Save Page &amp;As...</source>
+        <translation>保存为(&amp;A)</translation>
+    </message>
+    <message>
+        <source>Search expression:</source>
+        <translation type="unfinished">Search expression:</translation>
+    </message>
+    <message>
+        <source>Search for:</source>
+        <translation type="unfinished">Search for:</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>参数</translation>
+    </message>
+    <message>
+        <source>&amp;Settings...</source>
+        <translation>参数(&amp;S)</translation>
+    </message>
+    <message>
+        <source>Type:</source>
+        <translation>类型</translation>
+    </message>
+    <message>
+        <source>Use online content &amp;viewers to load unsupported content</source>
+        <translation type="unfinished">Use online content &amp;viewers to load unsupported content</translation>
+    </message>
+    <message>
+        <source>Use plugins to handle &amp;HTML5 audio and video</source>
+        <translation type="unfinished">Use plugins to handle &amp;HTML5 audio and video</translation>
+    </message>
+    <message>
+        <source>User:</source>
+        <translation>用户</translation>
+    </message>
+    <message>
+        <source>What to clear:</source>
+        <translation type="unfinished">What to clear:</translation>
+    </message>
+</context>
+</TS>
