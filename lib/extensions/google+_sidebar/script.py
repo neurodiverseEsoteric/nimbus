5df7b13ad0622f3319e7c265f0098bef1824265b
Daniel Sim <foxhead128@Mishiko.(none)>
2013-08-22 14:02:38 -0400

    Added a Google+ sidebar.

---
+++ lib/extensions/google+_sidebar/script.py
@@ -0,0 +1,11 @@
+name = "Google+"
+url = "https://plus.google.com/app/basic"
+clip = "plus.google"
+ua = "Mozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus Build/IMM76B) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.133 Mobile Safari/535.19"
+if not self.isCheckable():
+    self.setCheckable(True)
+    self.setChecked(True)
+if not self.parentWindow().hasSideBar(name):
+    self.parentWindow().addSideBar(name, url, clip, ua)
+else:
+    self.parentWindow().toggleSideBar(name)
