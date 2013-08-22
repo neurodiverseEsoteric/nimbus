cd2fd4f5ae13336aa4fd0fcb573bcc84e2affd3d
Daniel Sim <foxhead128@Mishiko.(none)>
2013-08-21 23:46:37 -0400

    Updated setup.py Bumped to 0.2.2

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 #! /usr/bin/env python3
 
+import sys
 import os
 import shutil
 from setuptools import setup
 
-version = "0.2.2pre"
+version = "0.2.2"
 try: f = open(os.path.join("lib", "version.txt"), "w")
 except: pass
 else:
@@ -15,16 +16,17 @@ files_to_copy = ("AUTHORS.txt", "LICENSE.md", "README.md", "THANKS.txt")
 for fname in files_to_copy:
     try: shutil.copy2(fname, "lib")
     except: pass
-setup(name='nimbus',
-      version=version,
-      description='Qt4 Web browser coded in Python 3, compatible with both',
-      author='Daniel Sim',
-      url='https://github.com/foxhead128/nimbus',
-      packages=['nimbus'],
-      package_dir={"nimbus": "lib"},
-      scripts=['nimbus'],
-      include_package_data=True
-     )
+if len(sys.argv) > 1:
+    setup(name='nimbus',
+          version=version,
+          description='Qt4 Web browser coded in Python 3, compatible with both',
+          author='Daniel Sim',
+          url='https://github.com/foxhead128/nimbus',
+          packages=['nimbus'],
+          package_dir={"nimbus": "lib"},
+          scripts=['nimbus'],
+          include_package_data=True
+         )
 for fname in files_to_copy:
     try: os.remove(os.path.join("lib", fname))
     except: pass
