4bf0d653f6673e9178b18ca62ed71354051bd915
Daniel Sim <foxhead128@Mishiko.(none)>
2013-09-03 01:52:58 -0400

    RSS parser has better exception handling now.

--- lib/rss_parser.py
+++ lib/rss_parser.py
@@ -29,6 +29,10 @@ def feedToHtml(url):
         return ""
     else:
         d = feedparser.parse(url)
+        try: feed_title = d.feed.title
+        except: feed_title = "Untitled"
+        try: icon = d.feed.icon
+        except: icon = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAIAAACQd1PeAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAB3RJTUUH3QgdBBMTEi/JQgAAABl0RVh0Q29tbWVudABDcmVhdGVkIHdpdGggR0lNUFeBDhcAAAAMSURBVAjXY/j//z8ABf4C/tzMWecAAAAASUVORK5CYII="
         items = []
         for entry in d.entries:
             try: content = entry.content
@@ -44,4 +48,4 @@ def feedToHtml(url):
             try: tags = ", ".join([tag.term for tag in entry.tags])
             except: tags = "None"
             items.append("<div class=\"item\">%(content)s<h2><a href=\"%(link)s\">%(title)s by %(author)s</a></h2>%(summary)s<b>Tags:</b> %(tags)s</div>" % {"content": content, "link": link, "title": title, "author": author, "tags": tags, "summary": summary})
-        return basic_html % {"icon": d.feed.icon, "title": d.feed.title, "content": "<hr/>".join(items)}
+        return basic_html % {"title": feed_title, "icon": icon, "content": "<hr/>".join(items)}
