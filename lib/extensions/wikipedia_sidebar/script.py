85fe3ac62197b4c3598e5a733f98eb90d1296ae3
Daniel Sim <foxhead128@Mishiko.(none)>
2013-09-12 20:57:55 -0400

    Wikipedia extension now uses the Nimbus sidebar API.

--- lib/extensions/wikipedia_sidebar/script.py
+++ lib/extensions/wikipedia_sidebar/script.py
@@ -1,21 +1,11 @@
-url = QUrl("http://m.wikipedia.org/")
+name = "Wikipedia"
+url = "http://m.wikipedia.org/"
+clip = "wikipedia"
+ua = None
 if not self.isCheckable():
     self.setCheckable(True)
     self.setChecked(True)
-try: self.parentWindow().wikipediaDock
-except:
-    self.parentWindow().wikipediaDock = QDockWidget(self.parentWindow())
-    self.parentWindow().wikipediaDock.setWindowTitle("Wikipedia")
-    self.parentWindow().wikipediaDock.setMaximumWidth(320)
-    self.parentWindow().wikipediaDock.setContextMenuPolicy(Qt.CustomContextMenu)
-    self.parentWindow().wikipediaDock.setFeatures(QDockWidget.DockWidgetClosable)
-    self.parentWindow().wikipediaView = WebView(self.parentWindow().wikipediaDock)
-    self.parentWindow().wikipediaView.windowCreated.connect(browser.activeWindow().addTab)
-    self.parentWindow().wikipediaView.load(url)
-    self.parentWindow().wikipediaDock.setWidget(self.parentWindow().wikipediaView)
-    self.parentWindow().addDockWidget(Qt.LeftDockWidgetArea, self.parentWindow().wikipediaDock)
-    self.parentWindow().tabifyDockWidget(self.parentWindow().sideBar, self.parentWindow().wikipediaDock)
+if not self.parentWindow().hasSideBar(name):
+    self.parentWindow().addSideBar(name, url, clip, ua)
 else:
-    self.parentWindow().wikipediaDock.setVisible(not self.parentWindow().wikipediaDock.isVisible())
-    if not "wikipedia" in self.parentWindow().wikipediaView.url().toString():
-        self.parentWindow().wikipediaView.load(url)
+    self.parentWindow().toggleSideBar(name)
