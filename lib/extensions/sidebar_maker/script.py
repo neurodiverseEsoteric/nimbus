6da2fd53e5237cbf4ae08a78cfbd06c52e8e0c83
Daniel Sim <foxhead128@Mishiko.(none)>
2013-09-14 17:02:07 -0400

    Updated sidebar maker to create an icon for sidebars.

--- lib/extensions/sidebar_maker/script.py
+++ lib/extensions/sidebar_maker/script.py
@@ -4,16 +4,20 @@ if name[1]:
     url = QInputDialog.getText(self, "URL", "Enter the URL of your sidebar here:")
     if url[1]:
         url = url[0]
-        path = os.path.join(settings.extensions_folder, name.lower() + "_sidebar")
-        if not os.path.exists(path):
-            os.mkdir(path)
-        try: f = open(os.path.join(path, "script.py"), "w")
+        common.sidebar_maker_path = os.path.join(settings.extensions_folder, name.lower() + "_sidebar")
+        if not os.path.exists(common.sidebar_maker_path):
+            os.mkdir(common.sidebar_maker_path)
+        common.sidebar_maker_icon_loader = WebView(None)
+        common.sidebar_maker_icon_loader.load(QUrl(url))
+        common.sidebar_maker_icon_loader.iconChanged.connect(lambda: common.sidebar_maker_icon_loader.icon().pixmap(16, 16).save(os.path.join(common.sidebar_maker_path, "icon.png")))
+        common.sidebar_maker_icon_loader.iconChanged.connect(common.sidebar_maker_icon_loader.deleteLater)
+        try: f = open(os.path.join(common.sidebar_maker_path, "script.py"), "w")
         except: pass
         else:
             try: f.write("""name = "%(name)s"
 url = "%(url)s"
 clip = "%(clip)s"
-ua = None
+ua = "Mozilla/5.0 (Linux; U; Android 2.3.5; en-us; HTC Vision Build/GRI40) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1"
 if not self.isCheckable():
     self.setCheckable(True)
     self.setChecked(True)
