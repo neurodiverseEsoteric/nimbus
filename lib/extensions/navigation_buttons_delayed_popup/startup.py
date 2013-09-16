69a88b7138ce4116112463d61298f77e33bec571
Daniel Sim <foxhead128@Mishiko.(none)>
2013-09-16 18:57:29 -0400

    Added two extensions that change the behavior of the navigation buttons.

---
+++ lib/extensions/navigation_buttons_delayed_popup/startup.py
@@ -0,0 +1,3 @@
+self.toolBar.widgetForAction(self.backAction).setPopupMode(QToolButton.DelayedPopup)
+self.toolBar.widgetForAction(self.forwardAction).setPopupMode(QToolButton.DelayedPopup)
+self.toolBar.widgetForAction(self.upAction).setPopupMode(QToolButton.DelayedPopup)
---
+++ lib/extensions/navigation_buttons_menu_only/startup.py
@@ -0,0 +1,3 @@
+self.toolBar.widgetForAction(self.backAction).setPopupMode(QToolButton.InstantPopup)
+self.toolBar.widgetForAction(self.forwardAction).setPopupMode(QToolButton.InstantPopup)
+self.toolBar.widgetForAction(self.upAction).setPopupMode(QToolButton.InstantPopup)
