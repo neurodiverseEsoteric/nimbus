14eb3ca49f6b536afd35cd51488ac4c0fe29c26e
foxhead128 <foxhead128@gmail.com>
2014-01-30 18:17:48 -0500

    Update.

--- lib/extensions/user_css_editor/startup.py
+++ lib/extensions/user_css_editor/startup.py
@@ -1,6 +1,6 @@
 self.styleMenuButton = QAction(self)
 self.styleMenuButton.setText(tr("User CSS Editor"))
-self.styleMenuButton.setShortcut("Ctrl+Shift+B")
+self.styleMenuButton.setShortcut("Alt+Shift+U")
 try: self.styleMenuButton.setIcon(QIcon(common.complete_icon("style")))
 except: traceback.print_exc()
 self.styleMenuButton.setCheckable(True)
