d44beed6887a5b4bd56e4884f53eb98b47e82388
foxhead128 <foxhead128@gmail.com>
2014-01-31 06:14:56 -0500

    Updated user css editor to only take plain text input.

--- lib/extensions/user_css_editor/startup.py
+++ lib/extensions/user_css_editor/startup.py
@@ -21,6 +21,7 @@ def togglestyleDock():
         mainWindow.styleDock.setContextMenuPolicy(Qt.CustomContextMenu)
         mainWindow.styleDock.setFeatures(QDockWidget.NoDockWidgetFeatures)
         mainWindow.styleEdit = QTextEdit(browser.activeWindow().styleDock)
+        mainWindow.styleEdit.setAcceptRichText(False)
         mainWindow.styleDock.setWindowTitle(tr("User CSS Editor"))
         mainWindow.styleDock.setWidget(browser.activeWindow().styleEdit)
         mainWindow.addDockWidget(Qt.RightDockWidgetArea, mainWindow.styleDock)
