d6f1f406359ee1b02dcfcdd2315f7007cb49abe9
foxhead128 <foxhead128@gmail.com>
2014-02-05 09:07:34 -0500

    Renamed sidebar_maker to add_sidebar. Google Calendar

---
+++ lib/extensions/google_calendar_sidebar/script.py
@@ -0,0 +1,11 @@
+name = "Google Calendar"
+url = "http://calendar.google.com/"
+clip = "google"
+ua = "Mozilla/5.0 (Linux; U; Android 2.3.5; en-us; HTC Vision Build/GRI40) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1"
+if not self.isCheckable():
+    self.setCheckable(True)
+    self.setChecked(True)
+if not self.parentWindow().hasSideBar(name):
+    self.parentWindow().addSideBar(name, url, clip, ua)
+else:
+    self.parentWindow().toggleSideBar(name)
