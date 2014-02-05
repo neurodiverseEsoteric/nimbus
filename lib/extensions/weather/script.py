1d7dc0f830b5c18c8ddacf936d3f5a7efa34fd2c
foxhead128 <foxhead128@gmail.com>
2014-02-05 09:47:19 -0500

    Weather extension depending on weather-util. Updated wayback shortcut.

---
+++ lib/extensions/weather/script.py
@@ -0,0 +1,18 @@
+try: common.weather_location
+except:
+    weatherLocation = settings.settings.value("general/WeatherLocation")
+    if not weatherLocation:
+        location = QInputDialog.getText(self, "Location", "Enter your current location here:")
+        if location[1]:
+            common.weather_location = location[0]
+            settings.settings.setValue("general/WeatherLocation", location[0])
+    else:
+        common.weather_location = weatherLocation
+try: common.weather_location
+except: pass
+else:
+    mainWindow = browser.activeWindow()
+    currentWidget = mainWindow.tabWidget().currentWidget()
+    stdout_handle = os.popen("weather %s" % (common.weather_location,))
+    forecast = stdout_handle.read()
+    QMessageBox.information(self, tr("Weather"), forecast)
