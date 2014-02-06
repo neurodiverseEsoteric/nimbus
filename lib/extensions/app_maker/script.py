cec15effafe6947e4117827e9ce8adafdaf83b2d
foxhead128 <foxhead128@gmail.com>
2014-02-06 11:47:30 -0500

    Updated app_maker

--- lib/extensions/app_maker/script.py
+++ lib/extensions/app_maker/script.py
@@ -7,10 +7,6 @@ if name[1]:
         common.app_maker_path = os.path.join(os.path.expanduser("~"), "Desktop")
         if not os.path.exists(common.app_maker_path):
             os.mkdir(common.app_maker_path)
-        common.app_maker_icon_loader = WebView(None)
-        common.app_maker_icon_loader.load(QUrl(url))
-        common.app_maker_icon_loader.iconChanged.connect(lambda: common.app_maker_icon_loader.icon().pixmap(16, 16).save(os.path.join(common.app_maker_path, "icon.png")))
-        common.app_maker_icon_loader.iconChanged.connect(common.app_maker_icon_loader.deleteLater)
         launcher_path = os.path.join(common.app_maker_path, name.lower().replace(" ", "_") + "_nimbus_app.desktop")
         try: f = open(launcher_path, "w")
         except: pass
