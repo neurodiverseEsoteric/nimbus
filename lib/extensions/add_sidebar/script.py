ceda0ccca5ef7130676b40d1cf43c7dc7e0790ac
foxhead128 <foxhead128@gmail.com>
2014-02-07 03:45:35 -0500

    Updated sidebar maker

--- lib/extensions/add_sidebar/script.py
+++ lib/extensions/add_sidebar/script.py
@@ -18,11 +18,13 @@ if name[1]:
 url = "%(url)s"
 clip = "%(clip)s"
 ua = "Mozilla/5.0 (Linux; U; Android 2.3.5; en-us; HTC Vision Build/GRI40) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1"
+style1 = os.path.join(settings.extensions_folder, self.name, "style.css")
+style = style1 if os.path.isfile(style1) else None
 if not self.isCheckable():
     self.setCheckable(True)
     self.setChecked(True)
 if not self.parentWindow().hasSideBar(name):
-    self.parentWindow().addSideBar(name, url, clip, ua)
+    self.parentWindow().addSideBar(name, url, clip, ua, style=style)
 else:
     self.parentWindow().toggleSideBar(name)""" % {"name": name, "url": url, "clip": url.split(".")[1]})
             except: pass
