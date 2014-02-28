38b107a571e0c6b06ffd25e8b13c67f308dd0674
foxhead128 <foxhead128@gmail.com>
2014-02-28 11:58:06 -0500

    Updated RainbowBB to latest version.

--- lib/extensions/rainbowbb/rainbowbb_gui.py
+++ lib/extensions/rainbowbb/rainbowbb_gui.py
@@ -32,13 +32,15 @@ html_escape_table = {
 def html_escape(text):
     return "".join(html_escape_table.get(c,c) for c in text)
 
-def create_gradient(cycle):
+def create_gradient(cycle, reverse=False, bounce=False):
     try: rainbowbb.cycles[cycle]
     except: return "transparent"
     else:
-        picked_cycle = rainbowbb.cycles[cycle]
-        increment = round(1/len(picked_cycle), 1)
-        gradient = "qlineargradient(x1:0, y1:0, x2:0, y2:1"
+        picked_cycle = rainbowbb.cycles[cycle][::-1] if reverse else rainbowbb.cycles[cycle]
+        if bounce:
+            picked_cycle = picked_cycle + picked_cycle[::-1]
+        increment = round(1/(len(picked_cycle)-1), 2)
+        gradient = "qlineargradient(x1:0, y1:0, x2:1, y2:0"
         counter = 0
         for color in picked_cycle:
             gradient += ", stop:%s #%s" % (counter, color)
@@ -70,12 +72,14 @@ class MainWindow(QMainWindow):
         self.addAction(self.closeWindowAction)
 
         self.reverseBox = QCheckBox("&Reverse", self)
+        self.reverseBox.clicked.connect(lambda: self.updateGradient())
         self.toolBar.addWidget(self.reverseBox)
 
         self.byWordBox = QCheckBox("By &word", self)
         self.toolBar.addWidget(self.byWordBox)
 
         self.bounceBox = QCheckBox("&Bounce", self)
+        self.bounceBox.clicked.connect(lambda: self.updateGradient())
         self.toolBar.addWidget(self.bounceBox)
         
         self.sizeList = QComboBox(self)
@@ -108,8 +112,10 @@ class MainWindow(QMainWindow):
         self.outputField.setReadOnly(True)
         self.setCentralWidget(self.outputField)
 
-    def updateGradient(self, index=0):
-        self.toolBar.setStyleSheet(stylesheet % (create_gradient(self.cycleList.itemText(index)),))
+    def updateGradient(self, index=None):
+        if not index:
+            index = self.cycleList.currentIndex()
+        self.toolBar.setStyleSheet(stylesheet % (create_gradient(self.cycleList.itemText(index), self.reverseBox.isChecked(), self.bounceBox.isChecked()),))
 
     def show(self):
         self.setVisible(True)
