180ae8321077bc46e40030ba66ced1cce7a2abf0
foxhead128 <foxhead128@gmail.com>
2014-09-17 12:31:46 -0400

    Updated githost for improved user friendliness.

--- lib/extensions/githost/script.py
+++ lib/extensions/githost/script.py
@@ -1,19 +1,23 @@
-try: common.git_image_repo
+try: common.git_repo
 except:
-    gitImageRepo = settings.settings.value("settings/GitImageRepo")
-    if not gitImageRepo:
-        location = QInputDialog.getText(self, "Location", "Enter the path of your Git repo here:")
-        if location[1]:
-            common.git_image_repo = location[0]
-            settings.settings.setValue("settings/GitImageRepo", location[0])
+    gitRepo = settings.settings.value("settings/GitRepo")
+    if not gitRepo:
+        gitRepo = "None"
+        QMessageBox.information(self, tr("Githost"), tr("Please select the directory of your Git repository."))
+        gitRepo = QFileDialog.getExistingDirectory(self, "Select directory", os.path.expanduser("~"))
+        if not os.path.isdir(gitRepo):
+            pass
+        else:
+            common.git_repo = gitRepo
+            settings.settings.setValue("settings/GitRepo", gitRepo)
     else:
-        common.git_image_repo = gitImageRepo
-try: common.git_image_repo
+        common.git_repo = gitRepo
+try: common.git_repo
 except: pass
 else:
-    fnames = QFileDialog.getOpenFileNames(None, tr("Select files..."), os.path.expanduser("~"), tr("All files (*)"))
+    fnames = QFileDialog.getOpenFileNames(self, tr("Select files to upload"), os.path.expanduser("~"), tr("All files (*)"))
     if len(fnames) > 0:
         for fname in fnames:
-            os.system("cp %s %s" % (fname, common.git_image_repo))
-        os.system("cd %s && git add . && git commit -m \"Added new images.\"" % (common.git_image_repo,))
-        os.system("cd %s && xterm -e \"git push --all\"" % (common.git_image_repo,))
+            os.system("cp %s %s" % (fname, common.git_repo))
+        os.system("cd %s && git add . && git commit -m \"Added new files (%s).\"" % (common.git_repo, ", ".join(fnames)))
+        os.system("cd %s && xterm -e \"git push --all\"" % (common.git_repo,))
