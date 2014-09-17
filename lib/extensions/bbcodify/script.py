22f781d825f446208cf14d3b84f7ff78dcbaed50
foxhead128 <foxhead128@gmail.com>
2014-09-17 00:28:36 -0400

    Bugfix for BBCodify

--- lib/extensions/bbcodify/script.py
+++ lib/extensions/bbcodify/script.py
@@ -7,7 +7,7 @@ title = mainWindow.tabWidget().currentWidget().windowTitle()
 if mainWindow.counter == 0:
     mainWindow.locationBar.setEditText("[url=%s]%s[/url]" % (url, title))
 elif mainWindow.counter == 1:
-    mainWindow.locationBar.setEditText("[url][img]%s[/img][/url]" % (url,))
+    mainWindow.locationBar.setEditText("[url=%s][img]%s[/img][/url]" % (url, url))
 #elif mainWindow.counter == 2:
 #    mainWindow.locationBar.setEditText("[img]" + url + "[/img]")
 mainWindow.counter += 1
