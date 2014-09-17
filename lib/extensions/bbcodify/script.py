fc17454400e63e932b95271a714ee4e7d5bc121e
foxhead128 <foxhead128@gmail.com>
2014-09-17 12:50:33 -0400

    Uppercase letters make the markup more distinguishable

--- lib/extensions/bbcodify/script.py
+++ lib/extensions/bbcodify/script.py
@@ -5,11 +5,11 @@ except:
 url = mainWindow.tabWidget().currentWidget().url().toString()
 title = mainWindow.tabWidget().currentWidget().windowTitle()
 if mainWindow.counter == 0:
-    mainWindow.locationBar.setEditText("[url=%s]%s[/url]" % (url, title))
+    mainWindow.locationBar.setEditText("[URL=%s]%s[/URL]" % (url, title))
 elif mainWindow.counter == 1:
-    mainWindow.locationBar.setEditText("[img]%s[/img]" % (url,))
+    mainWindow.locationBar.setEditText("[IMG]%s[/IMG]" % (url,))
 elif mainWindow.counter == 2:
-    mainWindow.locationBar.setEditText("[url=%s][img]%s[/img][/url]" % (url, url))
+    mainWindow.locationBar.setEditText("[URL=%s][IMG]%s[/IMG][/URL]" % (url, url))
 mainWindow.counter += 1
 if mainWindow.counter > 2:
     mainWindow.counter = 0
