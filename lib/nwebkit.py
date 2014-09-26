fcc77371ad68da8541ae0ca8be6845c8131d815e
foxhead128 <foxhead128@gmail.com>
2014-09-26 02:23:29 -0400

    Fixed error in text

--- lib/nwebkit.py
+++ lib/nwebkit.py
@@ -278,7 +278,7 @@ class WebPage(QWebPage):
         toolBar.setLayout(layout)
         w1 = custom_widgets.Expander(parent=toolBar)
         layout.addWidget(w1)
-        title = QLabel(parent=toolBar, text="<b>JavaScript Confirm:</b>")
+        title = QLabel(parent=toolBar, text="<b>JavaScript Prompt:</b>")
         layout.addWidget(title)
         tb.label = QLabel(parent=toolBar, text=msg)
         layout.addWidget(tb.label)
